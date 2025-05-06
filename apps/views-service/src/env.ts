bb7db1434dbb5c938b77332097c4732ef245ef3f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-05-06 22:44:07 +0300

    Setup: views-service: docker compose mongodb

--- apps/views-service/src/env.ts
+++ apps/views-service/src/env.ts
@@ -2,10 +2,18 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   PORT: z.string().default('3000'),
+  DATABASE_URL: z.string(),
+  DATABASE_NAME: z.string(),
+  DATABASE_USER: z.string(),
+  DATABASE_PASSWORD: z.string(),
 });
 
 type Env = z.infer<typeof envSchema>;
 
 export const env: Env = envSchema.parse({
   PORT: process.env.PORT || '3000',
+  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  DATABASE_USER: process.env.DATABASE_USER,
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
 });
